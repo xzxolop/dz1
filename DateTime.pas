d95f63385eba4c6ec627512d824d3c4e51b177a5
Voronin <lavaava44@gmail.com>
2022-10-11 22:41:20 +0300

    min to sec

--- DateTime.pas
+++ DateTime.pas
@@ -1,7 +1,5 @@
 ﻿
 begin
- var year:= readinteger('Введите год');
- if (year mod 4=0) and (year mod 100<>0)
-  or (year mod 100=0) and (year mod 400=0) then  write('Високосный')
- else write('Низокосный');
+ var min:=readinteger('Введите кол-во секунд');
+ print(60*min);
 end.
