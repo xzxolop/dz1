02c607f9af09252c7bffa47751c028d3141a0549
Voronin <lavaava44@gmail.com>
2022-10-12 17:16:58 +0300

    какой день ближе к новому году

--- DateTime.pas
+++ DateTime.pas
@@ -1,7 +1,9 @@
 ﻿
 begin
- var year:= readinteger('Введите год');
- if (year mod 4=0) and (year mod 100<>0)
-  or (year mod 100=0) and (year mod 400=0) then  write('Високосный')
- else write('Низокосный');
+ var (d1, d2):=readinteger2('Введите номер двух дней');
+ assert((d1 < 366) and (d2<366) and (d1>0) and (d2>0));
+ if d1 > d2 then
+  print(d1);
+ if d2 >d1 then
+   print(d2);
 end.
