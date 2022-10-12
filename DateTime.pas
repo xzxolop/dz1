ec3cbb15faebf7324a3718e9bcbd6aa817e610de
Voronin <lavaava44@gmail.com>
2022-10-12 17:01:22 +0300

    Revert "min to sec"
    This reverts commit d95f63385eba4c6ec627512d824d3c4e51b177a5.

--- DateTime.pas
+++ DateTime.pas
@@ -1,5 +1,7 @@
 ﻿
 begin
- var min:=readinteger('Введите кол-во секунд');
- print(60*min);
+ var year:= readinteger('Введите год');
+ if (year mod 4=0) and (year mod 100<>0)
+  or (year mod 100=0) and (year mod 400=0) then  write('Високосный')
+ else write('Низокосный');
 end.
