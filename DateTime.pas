82c2ff9ed39c8cc0d98dd41e88667f35de635875
Voronin <lavaava44@gmail.com>
2022-10-12 17:31:17 +0300

    колличество дней между 2 годами

--- DateTime.pas
+++ DateTime.pas
@@ -1,7 +1,17 @@
 ﻿
 begin
- var year:=readinteger('Введите номер года');
- if (year mod 4=0) and (year mod 100<>0)
-  or (year mod 100=0) and (year mod 400=0) then  write('366')
- else write('365');
+ 
+ var year1:=readinteger('Введите номер года');
+ var a:integer;
+ if (year1 mod 4=0) and (year1 mod 100<>0)
+  or (year1 mod 100=0) and (year1 mod 400=0) then  a:= 366
+ else a:=365;
+ 
+ var year2:=readinteger('Введите номер года');
+ var b:integer;
+ if (year2 mod 4=0) and (year2 mod 100<>0)
+  or (year2 mod 100=0) and (year2 mod 400=0) then  a:= 366
+ else b:=365;
+ 
+ print(abs(year1*a-year2*b));
 end.
